58a42b8062e88a3dd46f717adb475cc306dd3c46
Venky <ezio123arun@gmail.com>
2018-10-21 14:07:24 +0530

    Creating submit route

---
+++ routes/submit.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+var fs = require('fs');
+const path = require('path');
+
+fs.readFile(path.join(__dirname, '..', 'sampleFile.txt'), function(err, buf) {
+  eval(buf.toString());
+  router.get('/',callback);
+});
+
+module.exports = router;
